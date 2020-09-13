dd051fe89bf76257b47f9cbd534bdfc1856a50ba
toshick <toshickjazz@mac.com>
2020-09-13 11:16:02 +0900

    feat: input fontsize 16px

--- kitanu/.storybook/preview.js
+++ kitanu/.storybook/preview.js
@@ -1,11 +1,10 @@
-import '../node_modules/camaleao-design/css/reset.css';
+// import '../node_modules/camaleao-design/css/reset.css';
 import '../node_modules/camaleao-design/css/camaleao.css';
 import '../stories/story.scss';
 import '../assets/css/app.scss';
 
 import '../node_modules/camaleao-design/form/validation.ts';
 
-
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-}
+  actions: { argTypesRegex: '^on[A-Z].*' },
+};
--- kitanu/types/vue-shim.d.ts
+++ kitanu/types/vue-shim.d.ts
@@ -1,4 +1,4 @@
-import Vue from 'vue';
+import Vue from 'vue'; // import error おこる
 
 declare module '*.vue' {
   export default Vue;
